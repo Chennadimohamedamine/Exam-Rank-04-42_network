75b5bad7acaee0045d09a4db98637966c3c016fa
Mohamed Amine Chennadi <mochenna@e2r3p14.1337.ma>
2025-04-11 10:59:27 +0100

    is done

--- microshell.c
+++ microshell.c
@@ -1,71 +1,74 @@
-#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
-void err(char *str)
+int err(char *s, char *ss)
 {
-    while (*str)
-        write(2, str++, 1);
+    while (*s)
+        write(2, s++, 1);
+    if (ss[0] != 0)
+    {
+         while (*ss)
+            write(2, ss++, 1);
+    }
+    return (write(2,"\n",1));
 }
 
-int ft_cd(char **av, int i)
+int cd(char **av, int i)
 {
     if (i != 2)
-        return (err("error: cd: bad arguments\n"), 1);
+        return err("error: cd: bad arguments","");
     if (chdir(av[1]) == -1)
-        return (err("error: cd: cannot change directory to "), err(av[1]), err("\n"), 1);
-    return 0;
+        return err("error: cd: cannot change directory to ", av[1]);
+    return (0);
 }
 
-void setpip(int has_pip, int *fd, int end)
+void set_pip(int is, int *fd, int e)
 {
-    if (has_pip && (dup2(fd[end], end) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-    {
-        err("error: fatal\n"), exit(1);
-    }
+    if (is && (dup2(fd[e], e) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+        exit(err("error: fatal",""));
 }
 
-int exce(char **av, int i, char **env)
+int exe(char **av, int i, char **env)
 {
-    int has_pip, status, fd[2], pid;
-    has_pip = av[i] && !strcmp(av[i], "|");
+    int sts, fd[2], pid;
+    int is_pip = av[i] && !strcmp(av[i],"|");
 
-    if (!has_pip && !strcmp(*av, "cd"))
-        return (ft_cd(av, i));
-
-    if (has_pip && pipe(fd) == -1)
-        err("error: fatal\n"), exit(1);
-
+    if (!is_pip && !strcmp(*av,"cd"))
+        return (cd(av, i));
+    if (is_pip && pipe(fd) == -1)
+        exit(err("error: fatal",""));
     if ((pid = fork()) == -1)
-        err("error: fatal\n"), exit(1);
+        exit(err("error: fatal",""));
     if (!pid)
     {
         av[i] = 0;
-        setpip(has_pip, fd, 1);
-        if (!strcmp(*av, "cd"))
-            exit(ft_cd(av, i));
+        if (!strcmp(*av,"cd"))
+            exit(cd(av, i));
+        set_pip(is_pip, fd, 1);
         execve(*av, av, env);
-        err("error: cannot execute "), err(*av), err("\n"), exit(1);
+        exit(err("error: cannot execute ",*av));
     }
-    waitpid(pid, &status, 0);
-    setpip(has_pip, fd, 0);
-    return (WIFEXITED(status) && WEXITSTATUS(status));
+    waitpid(pid, &sts, 0);
+    set_pip(is_pip, fd, 0);
+    return (WIFEXITED(sts) && WEXITSTATUS(sts));
 }
-int main(int ac, char **av, char **env)
-{
-    (void)ac;
-    int i = 0;
-    int status = 0;
 
-    while (av[i])
+int main(int c,char **av, char **env)
+{
+    (void)c;
+    int i = 0;
+    int sts = 0;
+    while(av[i])
     {
         av += i + 1;
         i = 0;
-        while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+
+        while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
             i++;
         if (i)
-            status = exce(av, i, env);
+            sts = exe(av, i ,env);
     }
-    return (status);
+    return (sts);
 }
